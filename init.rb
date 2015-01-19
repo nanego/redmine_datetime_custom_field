bd4799bf21150983b6000ebfdad2ec4111483a38
Vincent Robert <vincent.robert@nanego.com>
2015-01-19 15:36:36 +0100

    Start with empty Redmine plugin

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_datetime_custom_field do
+  name 'Redmine Datetime Custom Field plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
