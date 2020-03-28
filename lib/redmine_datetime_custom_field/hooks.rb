6a6fecb14c922f519a2c8f2f0cf349a07be82dd3
Vincent Robert <vincent.robert@nanego.com>
2020-03-28 15:10:24 +0100

    Update stylesheet for Redmine 4.1.0

--- lib/redmine_datetime_custom_field/hooks.rb
+++ lib/redmine_datetime_custom_field/hooks.rb
@@ -3,7 +3,8 @@ module RedmineDatetimeCustomField
 
     # Add our css/js on each page
     def view_layouts_base_html_head(context)
-      javascript_include_tag('datetime_custom_field.js', plugin: 'redmine_datetime_custom_field')
+      stylesheet_link_tag("datetime_custom_field", :plugin => "redmine_datetime_custom_field") +
+          javascript_include_tag("datetime_custom_field", :plugin => "redmine_datetime_custom_field")
     end
 
   end
