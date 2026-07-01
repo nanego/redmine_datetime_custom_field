8f598440f9307478dacbc1641b07f2ab2e715756
Vincent <contact@vincent-robert.com>
2026-07-01 17:00:13 +0200

    Update CI config: add Redmine 7.0.0, drop 6.0.10, run on Ruby 4.0

--- spec/checksums_spec.rb
+++ spec/checksums_spec.rb
@@ -10,6 +10,6 @@ describe "Checksums" do
   it "checks core file application_helper checksums" do
     # several methods are overridden and should be reviewed if the checksum change
     # version 6.0.7, 6.1.0, 6.1.2 and 7.0.0 are OK
-    assert_checksum %w"64ef301df3413f4a35cbfeb9c9dddeaa 4d428a402e034b91951d6b18eb06b008 143bdcb83052700b686191107413f788 9e8d1cfa722af68547160a0b2ab50e82", "app/helpers/application_helper.rb"
+    assert_checksum %w"64ef301df3413f4a35cbfeb9c9dddeaa 4d428a402e034b91951d6b18eb06b008 143bdcb83052700b686191107413f788 9e8d1cfa722af68547160a0b2ab50e82 d9252db2e373b3c325933003743b80a1", "app/helpers/application_helper.rb"
   end
 end
